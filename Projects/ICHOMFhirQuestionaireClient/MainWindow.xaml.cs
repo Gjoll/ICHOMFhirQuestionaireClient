14995b04a86df42366d25e502afd64043eb9d516
Kurt Allen <KurtA@Gjoll.com>
2022-09-09 18:46:39 -0500

    .

--- Projects/ICHOMFhirQuestionaireClient/MainWindow.xaml.cs
+++ Projects/ICHOMFhirQuestionaireClient/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Web.WebView2.Core;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -13,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Path = System.IO.Path;
 
 namespace ICHOMFhirQuestionaireClient
 {
@@ -21,12 +23,42 @@ namespace ICHOMFhirQuestionaireClient
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool firstLoad = true;
+
         public MainWindow()
         {
             InitializeComponent();
             string curDir = Directory.GetCurrentDirectory();
-            //wbMain.Source = $"file://127.0.0.1/c$/path/to/the/file";
-            wbMain.Navigate(new Uri($"file:///{curDir}/WebPages/index.html", UriKind.RelativeOrAbsolute));
+            InitializeAsync();
+        }
+
+        // called from Window Constructor after InitializeComponent()
+        // note: the `async void` signature is required for environment init
+        async void InitializeAsync()
+        {
+            // must create a data folder if running out of a secured folder that can't write like Program Files
+            var env = await CoreWebView2Environment.CreateAsync(userDataFolder: Path.Combine(Path.GetTempPath(), "MarkdownMonster_Browser"));
+
+            // NOTE: this waits until the first page is navigated - then continues
+            //       executing the next line of code!
+            await webView.EnsureCoreWebView2Async(env);
+
+            //if (Model.Options.AutoOpenDevTools)
+            webView.CoreWebView2.OpenDevToolsWindow();
+
+            // Almost always need this event for something    
+            webView.NavigationCompleted += WebView_NavigationCompleted;
+
+            // set the initial URL
+            webView.Source = new Uri(
+                Path.Combine(Environment.CurrentDirectory,
+                            "WebPages",
+                            "index.html"));
+        }
+
+        private void WebView_NavigationCompleted(object? sender, 
+            Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs e)
+        {
         }
     }
 }
